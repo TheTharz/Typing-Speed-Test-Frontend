2a912934605dd34d8a243f8fcf76a9aa099219fe
thetharz <tharinduimalka915@gmail.com>
2023-08-04 22:13:10 +0530

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,50 +2,62 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <div className='w-full h-screen flex flex-row items-center justify-center gap-8 bg-lightgray '>
+    <div className='w-full h-screen flex flex-row items-center justify-center gap-8 bg-lightgray font-default'>
       {/* container of left side */}
-      <div>
-        <div>
-          <h3>Welcome</h3>
-          <p>Sign in to</p>
-          <p>Speed, Type, Excel</p>
+      <div className='m-4 p-4 bg-darkbackground rounded-lg w-[500px] text-yellow'>
+        <div className='p-2'>
+          <h3 className='text-2xl font-light'>Welcome!</h3>
+          <p className='text-3xl font-medium pt-6 pb-2'>Sign in to</p>
+          <p className='text-[16px] font-regular'>Speed, Type, Excel</p>
         </div>
-        <div>
+        <div className='py-4 px-2 text-[16px]'>
           <form>
-            <div>
-              <label for='email'>Username</label>
+            <div className='flex flex-col gap-1 my-2 py-2'>
+              <label for='email'>E-mail</label>
               <input
                 type='email'
                 name='email'
                 id='email'
-                placeholder='username'
+                placeholder='Enter your email'
+                className='border rounded px-4 py-2 border-border text-white focus:outline-none focus:border-lightgray bg-darkbackground'
               />
             </div>
-            <div>
+            <div className='flex flex-col gap-1 my-2 py-2'>
               <label for='password'>Password</label>
               <input
                 type='password'
                 name='password'
                 id='password'
-                placeholder='password'
+                placeholder='Enter your password'
+                className='border rounded px-4 py-2 border-border text-white focus:outline-none focus:border-lightgray bg-darkbackground'
               />
             </div>
-            <div>
-              <input type='checkbox' />
-              <label for='rememberMe'>Remember me</label>
-              <p>Forgot Password</p>
+            <div className='flex flex-row justify-between items-center text-[12px]'>
+              <div className='font-light space-x-1 flex items-center'>
+                <input type='checkbox' />
+                <label for='rememberMe'>Remember me</label>
+              </div>
+              <p className='text-lightgray'>Forgot Password?</p>
             </div>
-            <button type='submit'>Login</button>
+
+            <div className='flex w-full justify-center items-center'>
+              <button
+                type='submit'
+                className='w-[300px] bg-yellow text-darkbackground rounded-lg cursor-pointer m-4 p-2 text-16 font-medium'
+              >
+                Login
+              </button>
+            </div>
           </form>
         </div>
-        <div>
-          <p>Don't have an account</p>
-          <p>Register Now</p>
+        <div className='flex flex-row justify-center text-base gap-1'>
+          <p className='text-lightgray font-light'>Don't have an account?</p>
+          <p className='text-yellow font-semibold'>Register Now</p>
         </div>
       </div>
 
       {/* container of right side */}
-      <div>
+      <div className='w-[500px]'>
         <p>
           Redefine Your <br /> Typing Velocity: <br /> Where Words <br /> Meet
           Warp <br /> Speed
