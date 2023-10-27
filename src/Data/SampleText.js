1825fce5a2328f870080b78c0bb27865a62fe5e8
thetharz <tharinduimalka915@gmail.com>
2023-10-27 18:49:08 +0530

    added some sample paragraphs

---
+++ src/Data/SampleText.js
@@ -0,0 +1,8 @@
+const sampleText = [
+  'The quick brown fox jumps over the lazy dog Pack my box with five dozen liquor jugs Sphinx of black quartz judge my vow How razorback-jumping frogs can level six piqued gymnasts Mr Jock TV quiz PhD bags few lynx Jackdaws love my big sphinx of quartz The five boxing wizards jump quickly',
+  'She sells seashells by the seashore Unique New York unique New York you know you need unique New York A proper copper coffee pot Irish wristwatch Swiss wristwatch How can a clam cram in a clean cream can Fuzzy Wuzzy was a bear Fuzzy Wuzzy had no hair If two witches were watching two watches which witch would watch which watch',
+  'Black background brown background black background brown background Red leather yellow leather red leather yellow leather Truly rural truly rural Six slippery snails slid slowly seaward Which witch switched the Swiss wristwatches Freshly fried fresh flesh',
+  'Autumn leaves falling gently to the ground Golden harvest fields stretching far and wide Violet blooms in the meadow sway in the breeze Majestic mountains rise beyond the tranquil lake Eagles soar high above the rugged terrain',
+  "The moonlight shimmers on the still water Stars twinkle in the dark night sky Shadows dance in the forest's depths Crickets chirp in the cool evening air Owls hoot their lonesome songs in the distance",
+];
+export default sampleText;
